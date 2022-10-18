82c1ad4a52d5d6a8f1f39f11be5d3dc0c54e1345
Ilya <fox367213@mail.com>
2022-10-18 16:13:32 +0300

    Сделал первую задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите натуарльное число для степени");
+int stepen = int.Parse(Console.ReadLine()!);
+if (stepen >= 1)
+{
+    Console.WriteLine($"Число {A} в степени {stepen} = {GetPow(A,stepen)}");
+
+    int GetPow(int a, int b)
+    {
+        int number = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            number = number * a;
+        }
+        return number;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не прравильную степень");
+}
